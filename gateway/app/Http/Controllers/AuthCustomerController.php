951572519818b21cec544ad21bc6a00de593f586
Rizkhan Hadi <rizkhan.hadi07@gmail.com>
2021-12-24 13:23:09 +0700

    add customer authorization and getliststore

--- gateway/app/Http/Controllers/AuthCustomerController.php
+++ gateway/app/Http/Controllers/AuthCustomerController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Services\AuthServiceCustomer;
 use App\Services\ServiceCustomer;
 use App\Services\ServiceProduct;
+use App\Services\ServiceStore;
 use App\Services\ServiceTransaction;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use \App\Traits\ApiResponser;
 use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
 
 class AuthCustomerController extends BaseController
 {
@@ -18,13 +20,31 @@ class AuthCustomerController extends BaseController
     private $serviceCustomer;
     private $serviceProduct;
     private $serviceTransaction;
-    public function __construct(AuthServiceCustomer $authServiceCustomer, ServiceCustomer $serviceCustomer, ServiceProduct $serviceProduct, ServiceTransaction $serviceTransaction)
+    private $serviceStore;
+    private $TIME_EXPIRE = 3;
+    private $JWT_EXPIRED = false;
+    private $key = "asjlkdnaskjndjkawqnbdjkwbqdjknasljkmmndasjkjdnijkwqbduiqwbdojkawqnd";
+    public function __construct(AuthServiceCustomer $authServiceCustomer, ServiceCustomer $serviceCustomer, ServiceProduct $serviceProduct, ServiceTransaction $serviceTransaction, ServiceStore $serviceStore)
     {
         $this->authServiceCustomer = $authServiceCustomer;
         $this->serviceCustomer = $serviceCustomer;
         $this->serviceProduct = $serviceProduct;
         $this->serviceTransaction = $serviceTransaction;
+        $this->serviceStore = $serviceStore;
     }
+
+    private function auth($fcm)
+    {
+        $body = [
+            "fcm" => $fcm
+        ];
+
+        return json_decode($this->successResponse($this
+            ->authServiceCustomer
+            ->auth($body))
+            ->original, true);
+    }
+
 
     public function authCustomer(Request $request)
     {
@@ -78,6 +98,34 @@ class AuthCustomerController extends BaseController
             ->original, true);
     }
 
+    public function validationJWT($request)
+    {
+        $jwt = request()->header('Authorization');
+        $jwt = str_replace('Bearer ', '', $jwt);
+        $fcm = $request->header('fcm');
+        try {
+            $data = JWT::decode($jwt, $this->key, array('HS256'));
+            return [
+                "expired" => $this->JWT_EXPIRED,
+                "jwt" => $jwt,
+                "data" => (array)$data
+            ];
+        } catch (ExpiredException $ex) {
+            $data = $this->auth($fcm);
+            $payload = array(
+                "id" => $data['data']['id'],
+                "email" => $data['data']['email'],
+                "exp" => (round(microtime(true) * 1000) + ($this->TIME_EXPIRE * 60000))
+            );
+            $jwt = JWT::encode($payload, $this->key);
+            return [
+                "expired" => !$this->JWT_EXPIRED,
+                "data" => $payload,
+                "jwt" => $jwt
+            ];
+        }
+    }
+
     public function login(Request $request, $phone)
     {
 
@@ -100,9 +148,10 @@ class AuthCustomerController extends BaseController
             if($login["success"]){
                 $payload = array(
                     "id" => $login['data']['id'],
-                    "name" => $login['data']['name']
+                    "name" => $login['data']['name'],
+                    "exp" => (round(microtime(true) * 1000) + ($this->TIME_EXPIRE * 60000))
                 );
-                $jwt = JWT::encode($payload, env('APP_KEY'));
+                $jwt = JWT::encode($payload, $this->key);
                 $login['data']['jwt'] = $jwt;
                 return $login;
     
@@ -111,17 +160,18 @@ class AuthCustomerController extends BaseController
         
     }
 
-    public function getListProduct()
+    public function getListProduct(Request $request)
     {
+        $this->validationJWT($request);
         return json_decode($this->successResponse($this
             ->serviceProduct
             ->getListProduct())
             ->original, true);
     }
 
-    public function getListProductStore($id)
+    public function getListProductStore(Request $request,$id)
     {
-
+        $this->validationJWT($request);
         return json_decode($this->successResponse($this
             ->serviceProduct
             ->getListProductStore($id))
@@ -152,5 +202,13 @@ class AuthCustomerController extends BaseController
             ->serviceCustomer
             ->getLisCustomer())
             ->original, true);
+    }
+    public function getListStore(Request $request)
+    {
+        $this->validationJWT($request);
+        return json_decode($this->successResponse($this
+        ->serviceStore
+        ->getListStore())
+        ->original, true);
     }
 }
