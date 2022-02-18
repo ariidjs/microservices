10b23239da07606b07c705ec9c9a363a971c5983
razit <razituli@gmail.com>
2022-02-19 03:05:09 +0700

    trace1

--- gateway/app/Http/Controllers/AuthDriverController.php
+++ gateway/app/Http/Controllers/AuthDriverController.php
@@ -119,9 +119,9 @@ class AuthDriverController extends BaseController
     public function login(Request $request, $phone)
     {
 
-        // return "hello gateway";
+        return "hello gateway";
 
-       $response = json_decode($this->successResponse($this
+      return $response = json_decode($this->successResponse($this
             ->authServiceDriver
             ->checkPhone($phone))
             ->original, true);
