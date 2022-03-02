5b4592790947c99317448bfc7db53f50c610ce78
razit <razituli@gmail.com>
2022-03-02 15:29:00 +0700

    check

--- gateway/app/Http/Controllers/AuthAdminController.php
+++ gateway/app/Http/Controllers/AuthAdminController.php
@@ -612,7 +612,7 @@ class AuthAdminController extends BaseController
                 }
                 return ($a['saw'] > $b['saw']) ? -1 : 1;
             });
-            //return $listCustomer;
+            return $listCustomer;
 
             if(sizeof($listCustomer) > 1){
                 for($i=0;$i<1;$i++){
